c5a352792c79b11cdcf7681fe768f168e3a97d10
MohammedQasim <mqshaikh8@gmail.com>
2020-01-08 20:44:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@ const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+//   console.log("CONTENT LOADED!");
+//   console.log("Hello")
+ 
+// fetch('https://randomuser.me/api/')
+//   .then( res => res.json() )
+//   .then( data => {
+//     console.log("Hi") 
+//   })
+ 
+(params) => {
+  
+}
+// console.log("Sup?")
+
+function fetchData(){
+    fetch('https://randomuser.me/api/')
+    .then( resp => resp.json() )
+    .then( json => console.log(json))
+  
+}
+
+
+fetchData()
 });
+
+
